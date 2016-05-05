c3e87d524ff5c4176c9550eadb06d0d882896472
zanjs <root@zanjs.com>
2016-05-05 16:29:26 +0800

    Those who turn back never reach the summit.:angry:

--- 20160504/src/js/main.js
+++ 20160504/src/js/main.js
@@ -1,16 +1,16 @@
 ;(function () {
     $(function(){
        
-       var layers = $('.plaxify');
-        $.each(layers, function(index, layer){
-            $(layer).plaxify({
-            xRange: $(layer).attr('data-x') || 10,
-            yRange: $(layer).attr('data-y') || 20,
-            invert: $(layer).attr('data-invert') || false
-            });
-        });
+    //    var layers = $('.plaxify');
+    //     $.each(layers, function(index, layer){
+    //         $(layer).plaxify({
+    //         xRange: $(layer).attr('data-x') || 10,
+    //         yRange: $(layer).attr('data-y') || 20,
+    //         invert: $(layer).attr('data-invert') || false
+    //         });
+    //     });
         
-        $.plax.enable();
+    //     $.plax.enable();
 
         floorNnav();
         
--- 20160504/tmp/js/app.js
+++ 20160504/tmp/js/app.js
@@ -174,16 +174,16 @@ function floorNnav(){
 ;(function () {
     $(function(){
        
-       var layers = $('.plaxify');
-        $.each(layers, function(index, layer){
-            $(layer).plaxify({
-            xRange: $(layer).attr('data-x') || 10,
-            yRange: $(layer).attr('data-y') || 20,
-            invert: $(layer).attr('data-invert') || false
-            });
-        });
+    //    var layers = $('.plaxify');
+    //     $.each(layers, function(index, layer){
+    //         $(layer).plaxify({
+    //         xRange: $(layer).attr('data-x') || 10,
+    //         yRange: $(layer).attr('data-y') || 20,
+    //         invert: $(layer).attr('data-invert') || false
+    //         });
+    //     });
         
-        $.plax.enable();
+    //     $.plax.enable();
 
         floorNnav();
         
